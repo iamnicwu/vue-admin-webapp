98fe01a88ed7597ea4a5ae944a8dea31b9205537
Nick Wu <nwu@xogrp.com>
2019-12-30 11:22:51 +0800

    ver1.0

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
-const path = require('path')
+const path = require("path");
 const resolve = function(dir) {
-  return path.join(__dirname, dir)
-}
+  return path.join(__dirname, dir);
+};
 
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/vue-admin-webapp/' : '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
+    process.env.NODE_ENV === "production" ? "/vue-admin-webapp/" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
   lintOnSave: true, // 是否开启eslint保存检测
   productionSourceMap: false, // 是否在构建生产包时生成sourcdeMap
   chainWebpack: config => {
     config.resolve.alias
-      .set('@', resolve('src'))
-      .set('views', resolve('src/views'))
-    config.optimization.runtimeChunk('single')
+      .set("@", resolve("src"))
+      .set("views", resolve("src/views"));
+    config.optimization.runtimeChunk("single");
   },
   devServer: {
-    host: 'localhost',
-    port: '8080',
+    host: "localhost",
+    port: "8080",
     hot: true,
     open: true,
     overlay: {
@@ -31,9 +31,9 @@ module.exports = {
         changeOrigin: true,
         secure: false,
         pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
+          ["^" + process.env.VUE_APP_BASE_API]: ""
         }
       }
     }
   }
-}
+};
